79884b7bbd3eec95b904895bc15a2eb9de0ba87b
jemason <jemason@ece.uvic.ca>
2018-06-17 23:42:39 -0700

    Simple starting point

--- app/ClientApp/components/Layout.tsx
+++ app/ClientApp/components/Layout.tsx
@@ -1,16 +1,12 @@
 import * as React from 'react';
-import { NavMenu } from './NavMenu';
+import { TopNav } from './TopNav';
 
 export class Layout extends React.Component<{}, {}> {
     public render() {
-        return <div className='container-fluid'>
-            <div className='row'>
-                <div className='col-sm-3'>
-                    <NavMenu />
-                </div>
-                <div className='col-sm-9'>
-                    { this.props.children }
-                </div>
+        return <div className="container-fluid">
+            <TopNav />
+            <div className="container">
+                {this.props.children}
             </div>
         </div>;
     }
---
+++ app/ClientApp/components/TopNav.tsx
@@ -0,0 +1,30 @@
+﻿import * as React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+
+export class TopNav extends React.Component<{}, {}> {
+    public render() {
+        return <nav className="navbar navbar-inverse navbar-static-top">
+            <div className="container">
+                <div className="navbar-header">
+                    <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar3">
+                        <span className="sr-only">Toggle navigation</span>
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                    </button>
+                    <NavLink exact to={'/'} activeClassName="active" className="navbar-brand">
+                        <span className="glyphicon glyphicon-cog"></span>
+                        &nbsp; JEMML <span className="extended-title">Machine Learning</span>
+                    </NavLink>
+              </div>
+                    <div id="navbar3" className="navbar-collapse collapse">
+                        <ul className="nav navbar-nav navbar-right">
+                            <li className="active"><a href="#">Home</a></li>
+                            <li><a href="#">Analysis Builder</a></li>
+                            <li><a href="#">Documentation</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>;
+    }
+}
