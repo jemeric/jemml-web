e82e9a853da0275874d69e802e0322bbe741dfe9
jemeric <jericmason@gmail.com>
2018-06-24 15:49:23 -0700

    Some form styles

--- app/ClientApp/components/AnalysisType.tsx
+++ app/ClientApp/components/AnalysisType.tsx
@@ -2,7 +2,7 @@
 import { RouteComponentProps } from 'react-router-dom';
 
 export default class AnalysisType extends React.Component<{}, {}> {
-    private handleSelectChange(event: React.FormEvent<HTMLSelectElement>) {
+    private changeAnalysisType(event: React.FormEvent<HTMLSelectElement>) {
         console.log("CHAGE EVENT: ", event.currentTarget.value);
     }
 
@@ -10,7 +10,7 @@ export default class AnalysisType extends React.Component<{}, {}> {
         return <form>
             <h2>What sort of processing would you like to perform?</h2>
             <div className="form-group">
-                <select className="form-control" onChange={this.handleSelectChange}>
+                <select className="form-control" onChange={this.changeAnalysisType}>
                     <option value="PREPROCESS_DATA">Preprocess Data</option>
                     <option value="CLASSIFY_DATA">Classify Data</option>
                     <option value="TRAIN_PREPROCESSOR">Train Preprocessor</option>
@@ -18,6 +18,17 @@ export default class AnalysisType extends React.Component<{}, {}> {
                     <option value="GENERATE_EER">Generate EER</option>
                 </select>
             </div>
+            <div className="analysis-help">
+                <strong>Processing Configuration</strong>
+                <p>
+                    You will provide an input dateaset and select preprocessing to perform.
+                </p>
+                <strong>Output</strong>
+                <p>
+                    The preprocessing will be applied to your dataset and returned in a format of your choosing.
+                </p>
+            </div>
+            <button className="btn btn-default pull-right">Next Step</button>
         </form>;
     }
 }
