c5b99d360b3aa7c4adb5ad0e32c8bd9a3da62aa7
jemeric <jericmason@gmail.com>
2018-07-18 23:17:13 -0700

    Setup to override parent form submission

--- app/ClientApp/components/CustomForm.tsx
+++ app/ClientApp/components/CustomForm.tsx
@@ -4,19 +4,50 @@ import { ISubmitEvent, FormProps } from 'react-jsonschema-form';
 
 export default class CustomForm<T> extends Form<T> {
 
+    private baseSubmit: (e: React.FormEvent<T>) => {};
     constructor(props: FormProps<T>) {
         super(props);
         console.log("SUPER PROPS: ", props);
         console.log("this: ", this);
+        const that: any = this;
+        console.log("submit old: ", that.onSubmit);
+        this.baseSubmit = that.onSubmit;
+        that.onSubmit = this.submit.bind(this);
+
     }
 
-    onSubmit = (e: React.FormEvent<T>) => {
+    /* onSubmit = (e: React.FormEvent<T>) => {
         e.preventDefault();
         //super.onSubmit();
         console.log("ON SUBMIT CALLED?", e);
+        const state: any = this.state;
+        //state.formData = {};
+        //super.setState(state);
+        //super.forceUpdate(); // force update - re-render with new state
+        //_super1.
+        //that.setState(state);
+        //super.render();
+        //that.onChange(state.formData);
+        console.log("THIS: ", this);
+        //console.log("SUPER: ", super. .onSubmit(e));
+        console.log("FORM PROTOTYPE", Form.prototype);
+        console.log("STATE: ", this.state);
         // TODO validate with removeAdditional - pass along data having removed additional properties
-        //this.props.onSubmit();
+        //this.props.onSubmit(this.state);
+        //const s: any = super.;
 
+
+    } */
+
+    private submit(e: React.FormEvent<T>) {
+        e.preventDefault();
+        console.log("submit");
+        // TODO use custom validator to remove additional properties
+        //const state: any = this.state;
+        //state.formData = {};
+        //super.setState(state);
+        //super.forceUpdate(); // force update - re-render with new state
+        this.baseSubmit(e);
     }
 
     public render() {
